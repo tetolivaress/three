772c3d7803c42df0635f36c271911401d5b931c3
tetolivares <tetolivares@gmail.com>
2024-06-28 14:09:13 -0400

    testing grabVR

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,8 @@ import { GUI } from 'three/addons/libs/lil-gui.module.min.js'
 import { Floor, Box } from './models';
 import { setupXRControllers } from './controllers';
 import { OrbitControls } from 'three/examples/jsm/Addons.js';
-import { Box3, Vector3 } from 'three';
+import { Box3, Clock, Vector3 } from 'three';
+import GrabVR from 'grabvr'
 
 const { scene, renderer } = sceneRenderer(Camera);
 
@@ -12,7 +13,7 @@ console.log(size);
 
 // Camera.lookAt(Box.position)
 
-const { handleFirstController, handleSecondController } = setupXRControllers(scene, renderer);
+// const { handleFirstController, handleSecondController } = setupXRControllers(scene, renderer);
 
 const gui = new GUI();
 const BoxFolder = gui.addFolder('Box');
@@ -26,6 +27,17 @@ BoxFolder.add(Box.scale, 'y', .01, 32).name('Depth');
 const objects = [Floor, Box, spotLight, secondLight];
 scene.add(...objects);
 
+const grabVR = new GrabVR()
+grabVR.grabableObjects().push(Box);
+const controllerGrip0 = renderer.xr.getControllerGrip(0)
+const controllerGrip1 = renderer.xr.getControllerGrip(1)
+controllerGrip0.addEventListener("connected", (e: any) => {
+    controllerGrip0.add(controllerGrip0)
+    controllerGrip0.add(controllerGrip1)
+    grabVR.add(0, controllerGrip0, e.data.gamepad)
+    scene.add(controllerGrip0)
+})
+
 const orbitControls = new OrbitControls(Camera, renderer.domElement);
 
 renderer.xr.addEventListener('sessionstart', () => {
@@ -38,12 +50,14 @@ renderer.xr.addEventListener('sessionend', () => {
   Floor.visible = true;
 });
 
+const clock = new Clock();
+
 const animate = () => {
   renderer.setAnimationLoop(() => {
     orbitControls.update();
-    handleFirstController();
-    handleSecondController();
-
+    // handleFirstController();
+    // handleSecondController();
+    grabVR.update(clock.getDelta());
     renderer.render(scene, Camera);
   });
 };
