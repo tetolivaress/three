ba51004882052b76bb71f9bdf2d9d889de0e00af
tetolivares <tetolivares@gmail.com>
2024-07-15 13:42:53 -0400

    testing room

--- src/main.ts
+++ src/main.ts
@@ -1,36 +1,38 @@
-import { sceneRenderer, spotLight, secondLight, Camera, pointLight, spotLightHelper } from './utils';
-import { Floor, Box, Room } from './models';
-import { setupXRControllers } from './controllers';
-import { OrbitControls } from 'three/examples/jsm/Addons.js';
+import { sceneRenderer, spotLight, secondLight, Camera, pointLight, spotLightHelper } from './utils'
+import { Floor, Box, Room } from './models'
+import { setupXRControllers } from './controllers'
+import { OrbitControls } from 'three/examples/jsm/Addons.js'
 
-const { scene, renderer } = sceneRenderer(Camera);
+const { scene, renderer } = sceneRenderer(Camera)
 
-const { handleFirstController, handleSecondController } = setupXRControllers(scene, renderer);
+const { handleFirstController, handleSecondController } = setupXRControllers(scene, renderer)
 
 Box.scale.set(.3, .3, .3)
 
-const objects = [Floor, Box, spotLight, secondLight, pointLight, spotLightHelper, Room];
-scene.add(...objects);
+const objects = [Floor, Box, spotLight, secondLight, pointLight, spotLightHelper, Room]
+scene.add(...objects)
 
-const orbitControls = new OrbitControls(Camera, renderer.domElement);
+const orbitControls = new OrbitControls(Camera, renderer.domElement)
 
 renderer.xr.addEventListener('sessionstart', () => {
   if (renderer.xr.isPresenting) {
-    Floor.visible = false;
+    Floor.visible = false
+    Room.visible = false
   }
-});
+})
 
 renderer.xr.addEventListener('sessionend', () => {
-  Floor.visible = true;
-});
+  Floor.visible = true
+  Room.visible = true
+})
 
 const animate = () => {
   renderer.setAnimationLoop(() => {
-    orbitControls.update();
-    handleFirstController();
-    handleSecondController();
-    renderer.render(scene, Camera);
-  });
-};
+    orbitControls.update()
+    handleFirstController()
+    handleSecondController()
+    renderer.render(scene, Camera)
+  })
+}
 
-animate();
+animate()
