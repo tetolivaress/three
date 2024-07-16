6e5072c485cd00e510ce42ab8a1b9ab6d2fbf763
tetolivares <tetolivares@gmail.com>
2024-07-16 17:28:53 -0400

    ES6 modules and viteConfig

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import topLevelAwait from 'vite-plugin-top-level-await'
 
 export default defineConfig({
+  assetsInclude: ['**/*.hdr', '**/*.glb'],
   plugins: [topLevelAwait()],
   base: './',
 })
