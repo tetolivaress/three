0c96675ea37b8a49bb4514a927ad8730150f765f
tetolivares <tetolivares@gmail.com>
2024-07-15 20:29:51 -0400

    gltf working with async await

--- src/main.ts
+++ src/main.ts
@@ -3,36 +3,40 @@ import { Floor, Box, Room, getTable } from './models'
 import { setupXRControllers } from './controllers'
 import { OrbitControls } from 'three/examples/jsm/Addons.js'
 
-const { scene, renderer } = sceneRenderer(Camera)
-const table = await getTable()
-const { handleFirstController, handleSecondController } = setupXRControllers(scene, renderer)
+const init = async () => {
+  const { scene, renderer } = sceneRenderer(Camera)
+  const table = await getTable()
+  const { handleFirstController, handleSecondController } = setupXRControllers(scene, renderer)
 
-Box.scale.set(.3, .3, .3)
+  Box.scale.set(.3, .3, .3)
 
-const objects = [Floor, Box, spotLight, secondLight, pointLight, spotLightHelper, Room, table]
-scene.add(...objects)
+  const objects = [Floor, Box, spotLight, secondLight, pointLight, spotLightHelper, Room, table]
+  scene.add(...objects)
 
-const orbitControls = new OrbitControls(Camera, renderer.domElement)
+  const orbitControls = new OrbitControls(Camera, renderer.domElement)
 
-renderer.xr.addEventListener('sessionstart', () => {
-  if (renderer.xr.isPresenting) {
-    Floor.visible = false
-    Room.visible = false
-  }
-})
-
-renderer.xr.addEventListener('sessionend', () => {
-  Floor.visible = true
-  Room.visible = true
-})
-
-const animate = () => {
-  renderer.setAnimationLoop(() => {
-    orbitControls.update()
-    handleFirstController()
-    handleSecondController()
-    renderer.render(scene, Camera)
+  renderer.xr.addEventListener('sessionstart', () => {
+    if (renderer.xr.isPresenting) {
+      Floor.visible = false
+      Room.visible = false
+    }
   })
+
+  renderer.xr.addEventListener('sessionend', () => {
+    Floor.visible = true
+    Room.visible = true
+  })
+
+  const animate = () => {
+    renderer.setAnimationLoop(() => {
+      orbitControls.update()
+      handleFirstController()
+      handleSecondController()
+      renderer.render(scene, Camera)
+    })
+  }
+
+  animate()
 }
 
-animate()
+init()
