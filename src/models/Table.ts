4f189fea38479fbd6d8506c4685b1218c3dc387c
tetolivares <tetolivares@gmail.com>
2024-07-17 09:54:33 -0400

    gltf from assets

--- src/models/Table.ts
+++ src/models/Table.ts
@@ -1,10 +1,9 @@
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'; // Corrected import path
 import { Object3D } from 'three';
-import TableGLB from './table.glb'
 
 export const Table = async (): Promise<Object3D> => {
   const loader = new GLTFLoader();
-  const table = await loader.loadAsync(TableGLB);
+  const table = await loader.loadAsync('gltf/table.glb');
   table.scene.position.set(0, 0, 0);
   table.scene.scale.set(1, 1, 1);
   return table.scene;
